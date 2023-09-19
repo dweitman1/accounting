269abfdbabd32c5143676238135d3fa8f93bf2cc
Weitman <Daniel.Weitman@wsp.com>
2023-09-19 13:07:18 -0400

    Fixed bug where job summary total not multiplied

--- summarize.py
+++ summarize.py
@@ -40,7 +40,6 @@ companySummary = monthlySummary.drop(columns=["Rate"]).groupby(["Company"], sort
 fees = []
 for i in companyIndex:
     try:
-        
         fees.append(float(input(f"Enter fees for {i}: ")))
     except:
         fees.append(0)
@@ -50,18 +49,18 @@ companySummary.insert(8, "Total-Miles",companySummary["Total"] - companySummary[
 
 #-Totals-#
 totals = companySummary.sum(numeric_only=True)
-print(totals)
 
 #-Distribute OT premium per jobs per week-#
-a["Total"] = (a["Hours"] * a["Rate"]) + (mileageRate * a["Miles"])
+a["Total"] = (a["Hours"] * a["Rate"] * a["Multiplier"]) + (mileageRate * a["Miles"])
 weeklyCount = a.groupby(["Name", "Week"], sort=False)["Open"].count().reset_index().rename(columns={"Open":"Divisor"})
 otEntries = weeklySummary[weeklySummary["OT Hours"] > 0].drop(columns=["Hours", "Reg Hours", "Miles"]).reset_index()
-otEntries["Adder"] = (otEntries["Rate"] * 0.5) * otEntries["OT Hours"]
+otEntries["Adder"] = (otEntries["Rate"] * 0.5 * otEntries["Multiplier"]) * otEntries["OT Hours"]
 otEntries = otEntries.drop(columns=["Rate", "OT Hours"])
 otEntries = otEntries.merge(weeklyCount, how='left')
 otEntries["OT Adder"] = otEntries["Adder"] / otEntries["Divisor"]
 otEntries = otEntries.drop(columns=["Adder", "Divisor"])
 a = a.merge(otEntries, how='left').assign(Total=lambda d: d["Total"].add(d.pop("OT Adder"), fill_value=0))
+print("Job Summary Total: ", a["Total"].sum() + sum(fees))
 
 #-Calculate Job Summary-#
 jobSummary = a.drop(columns=["Rate", "Multiplier"]).groupby(["Job", "Fund", "Activity", "Facility"]).sum(numeric_only=True)
@@ -69,6 +68,9 @@ jobSummary.loc[jobSummary["Open"] > 0, "Open"] = "Open"
 jobSummary.loc[jobSummary["Open"] == 0, "Open"] = "Closed"
 jobSummary.loc[("Fees", 6, 75041, 6000), :] = ("Closed", 0, 0, totals["Fees"])
 jobSummary.set_index(["Open", "Hours", "Miles", "Total"])
+
+#-Set Total per Job Summary-#
+totals["Total"] = jobSummary["Total"].sum()
 
 #-Stylize-#
 style = {"border": "1px solid black", "background-color": "#abcdef"}
